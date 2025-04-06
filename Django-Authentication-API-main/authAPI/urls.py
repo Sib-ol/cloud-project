df6af4c0a8cadacf66e045515f712e0965f265ac
Sib-ol <126415555+Sib-ol@users.noreply.github.com>
2025-04-07 03:45:21 +0530

    Update urls.py

--- Django-Authentication-API-main/authAPI/urls.py
+++ Django-Authentication-API-main/authAPI/urls.py
@@ -1,40 +1,13 @@
-"""
-URL configuration for authAPI project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.http import JsonResponse
 
+# Optional welcome view at root URL
 def home(request):
     return JsonResponse({'message': 'Welcome to the Django Auth API!'})
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),  # root path
-    path('api/', include('account.urls')),  # adjust 'accounts' if your app has a different name
-]
-
-
-
-
-from django.contrib import admin
-from django.urls import path,include
-
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/user/', include('account.urls')),
+    path('', home),  # Root welcome route
+    path('api/user/', include('account.urls')),  # All user auth endpoints
 ]
