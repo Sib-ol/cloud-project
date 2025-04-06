293ef40d8cd1e62a4322441894e32cc488ea397e
Sib-ol <126415555+Sib-ol@users.noreply.github.com>
2025-04-07 03:38:42 +0530

    Update urls.py

--- Django-Authentication-API-main/account/urls.py
+++ Django-Authentication-API-main/account/urls.py
@@ -1,9 +1,9 @@
-from django.urls import path,include
-from account.views import UserRegistrationView,UserLoginView,UserProfileView,UserChangePasswordView,SendPasswordResetEmailView,UserPasswordResetView
+from django.urls import path
+from account.views import UserRegistrationView, UserLoginView, UserProfileView, UserChangePasswordView, SendPasswordResetEmailView, UserPasswordResetView
 
 urlpatterns = [
-    path('register/',UserRegistrationView.as_view(),name="register"),
-    path('login/',UserLoginView.as_view(),name="login"),
+    path('register/', UserRegistrationView.as_view(), name='register'),
+    path('login/', UserLoginView.as_view(), name='login'),
     path('profile/', UserProfileView.as_view(), name='profile'),
     path('changepassword/', UserChangePasswordView.as_view(), name='changepassword'),
     path('send-reset-password-email/', SendPasswordResetEmailView.as_view(), name='send-reset-password-email'),
